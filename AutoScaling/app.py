35c01b26c662d4453b5cd531bbad96a74c9657dd
tirth pipalia <56732761+tirth-pipalia@users.noreply.github.com>
2022-04-04 17:46:32 +0100

    Update app.py

--- AutoScaling/app.py
+++ AutoScaling/app.py
@@ -7,7 +7,7 @@ from auto_scaling.auto_scaling_stack import AutoScalingStack
 
 
 app = cdk.App()
-env_ME = cdk.Environment(account="620228650709", region="me-south-1")
+env_ME = cdk.Environment(account="12_digit_account_no", region="me-south-1")
 AutoScalingStack(app, "AutoScalingStack", env=env_ME)
 
 app.synth()
